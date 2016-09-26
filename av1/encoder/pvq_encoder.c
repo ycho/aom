8337c1bc9a5aabbc9d70fc75fa5c858777970eab
Yushin Cho <ycho@mozilla.com>
2016-09-26 14:38:11 -0700

    Turn off DC RDO of PVQ for chroma channels.
    In order to match the balance of PSNR BD-rate between AV1 and AV1/PVQ,
    given that PVQ itself is doing RDO while AV1's scalar quantization does not
    do any RDO (except Trellis coding), try not to discard any chroma DC info
    even though it is aganst PVQ's RDO.

--- av1/encoder/pvq_encoder.c
+++ av1/encoder/pvq_encoder.c
@@ -864,7 +864,7 @@ int od_pvq_encode(daala_enc_ctx *enc,
     n = OD_DIV_R0(abs(in[0] - ref[0]), dc_quant);
     if (n == 0) {
       out[0] = 0;
-    } else {
+    } else if (pli == 0) {
       int tell2;
       od_rollback_buffer dc_buf;
 
@@ -962,7 +962,7 @@ int od_pvq_encode(daala_enc_ctx *enc,
       n = OD_DIV_R0(abs(in[0] - ref[0]), dc_quant);
       if (n == 0) {
         out[0] = 0;
-      } else {
+      } else if (pli == 0) {
         int tell2;
         od_rollback_buffer dc_buf;
 
